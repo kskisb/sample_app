6522fd4a192c8ee36eff7a59650aecb2324e3094
kskisb <isbkosuke@gmail.com>
2024-08-19 13:19:14 +0000

    Complete edit with friendly forwarding

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -29,6 +29,11 @@ module SessionsHelper
     end
   end
 
+  # 渡されたユーザーがカレントユーザーであれば true を返す
+  def current_user?(user)
+    user && user == current_user
+  end
+
   def logged_in?
     !current_user.nil?
   end
@@ -44,5 +49,10 @@ module SessionsHelper
     forget(current_user)
     reset_session
     @current_user = nil
+  end
+
+  # アクセスしようとしたURLを保存する
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
