53801cad55015e81feb88ada3522ba10965fa10c
kskisb <isbkosuke@gmail.com>
2024-08-22 13:47:10 +0000

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -64,9 +64,10 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
 
-  # feed
+  # ユーザーのステータスフィードを返す
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id).includes(:user, image_attachment: :blob)
   end
 
   # ユーザーをフォローする
